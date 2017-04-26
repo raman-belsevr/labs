59710b71a5dec2b2aa697348aab0858598e2945a
Raman Grover <raman@awakenetworks.com>
2017-04-26 11:27:20 -0700

    RAS-13: 1) Added abstraction for sensor interfacing 2) Added implementation for acceleration-sensor 3) Added implementation for distance-sensor 4) Modified requirements.txt for for raspi-python

---
+++ raspi-python/raspi/sensor/acceleration_sensor.py
@@ -0,0 +1,27 @@
+from raspi.sensor import sensor_model
+from mpu6050 import mpu6050
+
+
+class AccelerationSensor(sensor_model=sensor_model.AbstractSensor):
+    """
+    https://pypi.python.org/pypi/mpu6050-raspberrypi/
+    """
+
+    def __init__(self):
+        self.sensor = mpu6050(0x68)
+
+    def get_ic(self):
+        return "acceleration sensor"
+
+    def get_reading(self):
+        """
+        Gets and returns the X, Y and Z values from the accelerometer.
+
+        If g is True, it will return the data in g
+        If g is False, it will return the data in m/s^2
+        Returns a dictionary with the measurement results.
+
+        :return:
+        """
+        accelerometer_data = self.sensor.get_accel_data(g=False)
+        return sensor_model.SensorReading(accelerometer_data)
---
+++ raspi-python/raspi/sensor/distance_sensor.py
@@ -0,0 +1,56 @@
+from raspi.sensor import sensor_model
+from gpiozero import DistanceSensor
+
+
+class UltraSonicDistanceSensor(sensor_model.AbstractSensor):
+
+    """
+    Ultrosound-based distance sensor
+    """
+
+    def __init__(self, echo=17, trigger=4, threshold_distance=0.5, max_distance=2):
+        self.ultrasonic = DistanceSensor(echo, trigger, threshold_distance, max_distance)
+        DistanceSensor.__configure__()
+
+    def get_ic(self):
+        return "ultra_sonic_distance_sensor"
+
+    def get_reading(self):
+        input_value = self.ultrasonic.distance
+        return sensor_model.SensorReading(input_value)
+
+    def wait_for_in_range(self):
+        """
+        Blocking call that waits until the sensor detects distance in range
+        :return:
+        """
+        self.ultrasonic.wait_for_in_range()
+        print("In range")
+
+    def wait_for_out_of_range(self):
+        """
+        Blocking call that waits until the sensor detects being out of range
+        :return:
+        """
+        self.ultrasonic.wait_for_out_of_range()
+        print("Out of range")
+
+    def when_in_range(self, f):
+        """
+        Asynchronous call with a configure callback that is invoked when the sensor
+        detects being in range
+        :param f: callback
+        """
+        self.ultrasonic.when_in_range = f
+
+    def when_out_of_range(self, f):
+        """Asynchronous call with a configure callback that is invoked when the sensor
+           detects being out of range
+           :param f: callback
+        """
+        self.ultrasonic.when_out_of_range = f
+
+
+
+
+
