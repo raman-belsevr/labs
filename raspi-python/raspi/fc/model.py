758badfc6531965929a970ef10a6955069e00589
raman-belsevr <raman-belsevr@users.noreply.github.com>
2017-05-02 19:05:56 -0700

    WIP: RAS-17 Implement MWii compliant flight controller adapter to interface with the flight controller hardware (#11)

---
+++ raspi-python/raspi/fc/model.py
@@ -0,0 +1,106 @@
+import serial
+import time
+import threading
+from raspi.fc import multiwii_serial_protocol
+from logging import Logging
+
+
+class FlightController:
+
+    def __init__(self, name, port):
+
+        ########################################
+        # Initialization of controller options #
+        ########################################
+
+        self.name = name     # Name of the flight controller hardware chip
+        self.started = True  # Indicating whether flight controller is operational or not
+
+        self.ATT = 0      # Ask the attitude of the multicopter
+        self.ATT = 0      # Ask the altitude of the multicopter
+        self.SET_RC = 1   # Set rc command
+        self.PRINT = 0    # Print data to terminal
+        self.timeMSP = 0.02 # sleeping interval in spin-loop
+
+        ##################################
+        # Communication with serial port #
+        ##################################
+        self.port = port
+        self.ser = serial.Serial()
+        self.configure_serial_port(self.ser)
+
+
+        ###############################
+        # Select Serial Protocol
+        ##############################
+        self.protocol = multiwii_serial_protocol.MultiwiiSerialProtocol()
+
+        ###############################
+        # Initialize Global Variables
+        ###############################
+        self.latitude = 0.0
+        self.longitude = 0.0
+        self.altitude = -0
+        self.heading = -0
+        self.timestamp = -0
+        self.gpsString = -0
+        self.numSats = -0
+        self.accuracy = -1
+        self.beginFlag = 0
+        self.roll = 0
+        self.pitch = 0
+        self.yaw = 0
+        self.throttle = 0
+        self.angx = 0.0
+        self.angy = 0.0
+        self.m1 = 0
+        self.m2 = 0
+        self.m3 = 0
+        self.m4 = 0
+        self.message = ""
+        self.ax = 0
+        selfay = 0
+        self.az = 0
+        self.gx = 0
+        self.gy = 0
+        self.gz = 0
+        self.magx = 0
+        self.magy = 0
+        self.magz = 0
+        self.elapsed = 0
+        self.flytime = 0
+        self.numOfValues = 0
+        self.precision = 3
+        self.rcData = [1500, 1500, 1500, 1500]  # order -> roll, pitch, yaw, throttle
+
+        self.loopThread = threading.Thread(target=self.loop)
+        if self.ser.isOpen():
+            print("Wait 5 sec for calibrate Multiwii")
+            time.sleep(5)
+            self.loopThread.start()
+
+    def configure_serial_port(self, ser):
+        ser.port = self.port
+        ser.baudrate = 115200
+        ser.bytesize = serial.EIGHTBITS
+        ser.parity = serial.PARITY_NONE
+        ser.stopbits = serial.STOPBITS_ONE
+        ser.timeout = 0
+        ser.xonxoff = False
+        ser.rtscts = False
+        ser.dsrdtr = False
+        ser.writeTimeout = 2
+
+        try:
+            ser.open()
+        except Exception as e:
+             Logging.logger.error("Unable to open serial port % " % str(e))
+             exit()
+
+    def stop(self):
+        self.started = False
+
+    def set_rc(self):
+        self.protocol.send_data(8, self.protocol.CMD2CODE["MSP_SET_RAW_RC"], self.rcData)
+        time.sleep(self.timeMSP)
+        # print self.rcData
---
+++ raspi-python/raspi/fc/multiwii_serial_protocol.py
@@ -0,0 +1,79 @@
+import struct		# for decoding data strings
+
+
+class MultiwiiSerialProtocol:
+
+    def __init__(self):
+        ###############################
+        # Multiwii Serial Protocol
+        # Hex value for MSP request
+        ##############################
+        self.BASIC = "\x24\x4d\x3c\x00"  # MSG Send Header (to MultiWii)
+        self.MSP_IDT = self.BASIC + "\x64\x64"  # MSG ID: 100
+        self.MSP_STATUS = self.BASIC + "\x65\x65"  # MSG ID: 101
+        self.MSP_RAW_IMU = self.BASIC + "\x66\x66"  # MSG ID: 102
+        self.MSP_SERVO = self.BASIC + "\x67\x67"  # MSG ID: 103
+        self.MSP_MOTOR = self.BASIC + "\x68\x68"  # MSG ID: 104
+        self.MSP_RC = self.BASIC + "\x69\x69"  # MSG ID: 105
+        self.MSP_RAW_GPS = self.BASIC + "\x6A\x6A"  # MSG ID: 106
+        self.MSP_ATTITUDE = self.BASIC + "\x6C\x6C"  # MSG ID: 108
+        self.MSP_ALTITUDE = self.BASIC + "\x6D\x6D"  # MSG ID: 109
+        self.MSP_BAT = self.BASIC + "\x6E\x6E"  # MSG ID: 110
+        self.MSP_COMP_GPS = self.BASIC + "\x71\x71"  # MSG ID: 111
+        self.MSP_SET_RC = self.BASIC + "\xC8\xC8"  # MSG ID: 200
+
+        self.CMD2CODE = {
+            # Getter
+            'MSP_IDENT': 100,
+            'MSP_STATUS': 101,
+            'MSP_RAW_IMU': 102,
+            'MSP_SERVO': 103,
+            'MSP_MOTOR': 104,
+            'MSP_RC': 105,
+            'MSP_RAW_GPS': 106,
+            'MSP_COMP_GPS': 107,
+            'MSP_ATTITUDE': 108,
+            'MSP_ALTITUDE': 109,
+            'MSP_ANALOG': 110,
+            'MSP_RC_TUNING': 111,
+            'MSP_PID': 112,
+            'MSP_BOX': 113,
+            'MSP_MISC': 114,
+            'MSP_MOTOR_PINS': 115,
+            'MSP_BOXNAMES': 116,
+            'MSP_PIDNAMES': 117,
+            'MSP_WP': 118,
+            'MSP_BOXIDS': 119,
+
+            # Setter
+            'MSP_SET_RAW_RC': 200,
+            'MSP_SET_RAW_GPS': 201,
+            'MSP_SET_PID': 202,
+            'MSP_SET_BOX': 203,
+            'MSP_SET_RC_TUNING': 204,
+            'MSP_ACC_CALIBRATION': 205,
+            'MSP_MAG_CALIBRATION': 206,
+            'MSP_SET_MISC': 207,
+            'MSP_RESET_CONF': 208,
+            'MSP_SET_WP': 209,
+            'MSP_SWITCH_RC_SERIAL': 210,
+            'MSP_IS_SERIAL': 211,
+            'MSP_DEBUG': 254,
+        }
+
+    def send_data(self, data_length, code, data):
+        checksum = 0
+        total_data = ['$', 'M', '<', data_length, code] + data
+        for i in struct.pack('<2B%dh' % len(data), *total_data[3:len(total_data)]):
+            checksum = checksum ^ ord(i)
+
+        total_data.append(checksum)
+
+        try:
+            b = None
+            b = self.ser.write(struct.pack('<3c2B%dhB' % len(data), *total_data))
+        except Exception as ex:
+            print
+            'send data error'
+            print(ex)
+        return b
