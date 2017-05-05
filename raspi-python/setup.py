099432a459847c7f36997485e42a69d111a1c150
Raman Grover <raman@awakenetworks.com>
2017-05-05 15:26:08 -0700

    WIP-2: preparing for build

--- raspi-python/setup.py
+++ raspi-python/setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name = 'raspi',
-    packages = ['raspi', 'raspi.fc', 'raspi.sensor'],
+    packages = ['raspi', 'raspi.fc', 'raspi.sensor', 'RPi'],
     version = '0.1',
     author = 'raman grover',
     author_email='raman@belsevr.com',
