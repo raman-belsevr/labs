c13a174d091ee9a4b92693475c8f4cbb9c79cedb
raman-belsevr <raman-belsevr@users.noreply.github.com>
2017-05-07 14:39:24 +0000

    prepare pilot module for build (#22)

--- drone-python/drone/util.py
+++ drone-python/drone/util.py
@@ -1,10 +1,6 @@
-from drone import drone_model
-from raspi.sensor import distance_sensor
-from raspi.sensor import camera_sensor
-from raspi.sensor.distance_sensor import UltraSonicDistanceSensor
 
-
-@staticmethod
 class Util:
+
+    @staticmethod
     def sensor_id(sensor, direction):
         return sensor + "_" + direction
--- pilot-python/pilot/pilot_model.py
+++ pilot-python/pilot/pilot_model.py
@@ -1,5 +1,70 @@
 from enum import Enum
 from itertools import repeat
+from raspi.raspi_logging import get_logger
+from drone.drone_system import DroneSystem
+
+
+class Pilot:
+    logger = get_logger(__name__)
+
+    def __init__(self):
+        Pilot.logger.info("initializing pilot...")
+        self.drone = DroneSystem("my_drone")
+        self.atc = None
+        self.flight_state = FlightState()
+        Pilot.logger.info("initialized pilot")
+
+    @staticmethod
+    def validate_state(state):
+        return state.any_errors == "false"
+
+    def status(self):
+        return self.drone.get_status()
+
+    def execute_flight_sequence(self, flight_sequence):
+        self.logger.info("executing flight sequence %s", flight_sequence)
+
+    def fly(self, data):
+        # provide 100 as delta thrust
+        self.drone.control_system.change_thrust(100)
+
+    def climb(self):
+        self.drone.control_system.change_thrust(100)
+
+    def descent(self, delta):
+        self.drone.control_system.change_thrust(-100)
+
+    def cruise(self, speed):
+        # form a mix of commands to control system
+        pass
+
+    def steer_left(self, delta):
+        self.drone.control_system.change_aileron(-delta)
+        # form a mix of commands to control system
+        pass
+
+    def steer_right(self, delta):
+        # form a mix of commands to control system
+        self.drone.control_system.change_aileron(delta)
+        pass
+
+    def hover(self):
+        # form a mix of commands to control system
+        self.drone.control_system.set_flight_control(1500, 1500, 1500, 1500)
+        pass
+
+    def land(self, data):
+        # form a mix of commands to control system
+        pass
+
+    def take_image(self):
+        self.drone.media_system.image_front_left()
+
+    def start(self):
+        self.scheduler.schedule(self.fly)
+
+    def end(self, data):
+        self.scheduler.cancel(self.fly)
 
 
 class Constants:
